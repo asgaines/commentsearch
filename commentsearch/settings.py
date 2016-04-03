a77b62d35d65356fbd5618ba728d21ff42deed48
asgaines <andrew.s.gaines@gmail.com>
2016-04-03 17:58:17 -0600

    Move review article link to settings file

--- commentsearch/settings.py
+++ commentsearch/settings.py
@@ -104,6 +104,8 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+REVIEW_ARTICLE_LINK = 'http://www.dcrainmaker.com/2015/01/stryd-first-running.html'
+
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
