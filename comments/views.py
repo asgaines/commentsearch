6ecda5e71cc9b4d0436024492407dae876eb2833
asgaines <andrew.s.gaines@gmail.com>
2016-04-03 18:40:08 -0600

    Implement own functionality without library/framework support

--- comments/views.py
+++ comments/views.py
@@ -1,6 +1,5 @@
 from django.db.models import Q
 from django.shortcuts import render
-from django.http import HttpResponse
 
 from commentsearch import settings
 from comments.models import Comment
@@ -9,6 +8,12 @@ def index(request):
     comment_matches = []
     if request.method == 'POST':
         search_terms = [term.strip() for term in request.POST.get('search-terms').split(',')]
-        comment_matches = Comment.objects.filter(reduce(lambda x, y: x | y, [Q(body__icontains=term) for term in search_terms]))
+
+        # Original implementation, unsure if in violation of Criteria point #2 (uses Django ORM framework for SQL generation)
+        # comment_matches = Comment.objects.filter(reduce(lambda x, y: x | y, [Q(body__icontains=term) for term in search_terms]))
+
+        # To play it safe, here is my own implementation
+        comment_matches = set([comment for comment in Comment.objects.all() for term in search_terms if term.lower() in comment.body.lower()])
+
     context = {'method': request.method, 'comment_matches': comment_matches, 'link': settings.REVIEW_ARTICLE_LINK}
     return render(request, 'comments/index.html', context)
